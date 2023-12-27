8e07dedcafff7743dcc9f92833d0f13c4060c0e9
Blannon <ngogeblannon10@gmail.com>
2023-12-27 20:22:55 +0300

    Updated category section

---
+++ app/src/main/java/com/blannon_network/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.blannon_network
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blannon_network.tamurecipes.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
